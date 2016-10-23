a3e965fade45ecd5d8a6d7cc9adc06fda1b4df03
rcbrett <rcbrett@gmail.com>
2016-10-23 08:52:07 -0700

    Added ability to specify internal dilution Fixed bugs

--- Au_interval.R
+++ Au_interval.R
@@ -1,44 +1,55 @@
-# libraries ---------------------------------------------------------------
-
-library(ggplot2)
-library(dplyr)
-library(zoo)
-
-cutoff <- 0
-topcut <- 1000
-
-
-
-# load data ---------------------------------------------------------------
-DHinterval <- read.csv("data/Au_Interval.csv", header = TRUE)
-
-
-# Interval identification -------------------------------------------------
-
-# get a columns where there are 1's if Au_ppm > 0.2, and 0's if not 
-DHinterval$ints <- findInterval(DHinterval$Au_ppm, cutoff)
-
-
-# make a dataframe with start positions of runs of 1's 
-starts <- filter(DHinterval, Au_ppm >= cutoff & ((lag(ints) == 0 | is.na(lag(ints)))& lead(ints) == 1)) 
-starts$position <- rep('start', dim(starts)[1])
-
-# make a dataframe with end positions of runs of 1's 
-ends <- filter(DHinterval, Au_ppm >= cutoff & (lag(ints) == 1 & (lead(ints) == 0) | is.na(lead(ints)))) 
-ends$position <- rep('end', dim(ends)[1])
-
-# make a dataframe with groups of runs
-Au_run <- rbind(starts, ends)
+  
+  # libraries ---------------------------------------------------------------
+  
+  library(ggplot2)
+  library(dplyr)
+  library(zoo)
+  
+  cutoff <- 0.4
+  topcut <- 3
+  internal_dilution <- 10
+  
+  
+  # load data ---------------------------------------------------------------
+  DHinterval <- read.csv("data/Au_Interval.csv", header = TRUE)
+  
+  
+  # Interval identification -------------------------------------------------
+  
+  # get a columns where there are 1's if Au_ppm > cutoff, and 0's if not 
+  DHinterval$ints <- findInterval(DHinterval$Au_ppm, cutoff)
+  
+  # Accounts for specified internal dilution
+  consecutive_intValues <- rle(DHinterval$ints) # creates vector with lengths of consecutive runs and the values of those runs in this case it is 0 or 1
+  DHinterval$dilutionCondition <- rep(consecutive_intValues$lengths >= internal_dilution,times = consecutive_intValues$lengths) # replicates
+  DHinterval$update <- DHinterval$ints
+  DHinterval$update[DHinterval$ints == 0 & DHinterval$dilutionCondition == "FALSE"] <- 1
+  
+  
+  # make a dataframe with start positions of runs of 1's 
+  # starts <- filter(DHinterval, Au_ppm >= cutoff & ((lag(update) == 0 | is.na(lag(update)))& lead(update) == 1)) 
+  starts <- filter(DHinterval, ((lag(update) == 0 | is.na(lag(update)))& lead(update) == 1)) 
+  starts$position <- rep('start', dim(starts)[1])
+  
+  # make a dataframe with end positions of runs of 1's 
+  # ends <- filter(DHinterval, Au_ppm >= cutoff & (lag(update) == 1 & (lead(update) == 0) | is.na(lead(update)))) 
+  ends <- filter(DHinterval, (lag(update) == 1 & (lead(update) == 0) | is.na(lead(update))))
+  ends$position <- rep('end', dim(ends)[1])
+  
+  # make a dataframe with groups of runs
+  Au_run <- rbind(starts, ends)
 
 # order them by position
 Au_run <- Au_run[with(Au_run, order(From)), ] 
-Au_run$group <- rep(1:(dim(Au_run)[1]/2), each=2)
+#Au_run$group <- rep(1:(dim(Au_run)[1]/2), each=2)
+x=seq(1,(dim(starts)[1]),1)
+Au_run$group <- sort(c(x,x))
 
 # join Au_run with original dataset
 DHinterval_grouped <- left_join(DHinterval, Au_run)
 
 # change group to 0 if int is 0
-DHinterval_grouped$group[DHinterval_grouped$ints == 0] <- 0
+DHinterval_grouped$group[DHinterval_grouped$update == 0] <- 0
 
 # fill in NA's with last values (singles will get filled in with 0?) 
 new_groups <- na.locf(DHinterval_grouped$group)
@@ -72,7 +83,7 @@ write.csv(Au_means, "Au_means.csv", row.names = FALSE)
 # plot means on a bar -----------------------------------------------------
 Au_ppm_plot <- ggplot(DHinterval_grouped) +
   geom_line(aes(y=Au_ppm, x=From)) +
-  geom_line(aes(y = -2, x=From, colour = log(Au_ppm)), size=2) +
+  geom_line(aes(y = 0, x=From, colour = log(Au_ppm)), size=2) +
   coord_flip() +
   scale_x_reverse() +
   scale_colour_gradientn(colours = rev(rainbow(5)))
@@ -87,4 +98,4 @@ probPlot
 Au_ppm_plot
 ggsave('Au_ppm.pdf', Au_ppm_plot)
 ggsave('ProbPlot.pdf', probPlot)
-
+Au_means
